036eac7a23bb5c1f757591ae542bfd0633415bac
yaanno <janos.hardi@kinja.com>
2022-07-31 17:50:10 +0200

    fun with sound

--- src/features/parking/Parking.tsx
+++ src/features/parking/Parking.tsx
@@ -2,10 +2,12 @@
 import { useAppSelector, useAppDispatch } from '../../app/hooks';
 import { park, remove, selectCount, getAllSlots, updatePrice, getCheapestSlot } from './parkingSlice';
 import { uniqueId } from '../../util';
-import { CircularProgress, Container, Button, SimpleGrid, GridItem, Tooltip, Center, Spinner, Avatar, AvatarGroup, Box } from '@chakra-ui/react'
+import { Switch, CircularProgress, Container, Button, SimpleGrid, GridItem, Tooltip, Center, Spinner, Avatar, AvatarGroup, Box, Heading } from '@chakra-ui/react'
 import Tommy from '../../tommy.jpg';
 import Sonny from '../../sonny.jpg';
 import { useState } from 'react';
+import audiofile from '../../sound/gta.mp3';
+import TommyEasy from '../../sound/tommy-easy.mp3';
 
 const CURR = '$';
 
@@ -23,7 +25,7 @@ const ParkingSlotItem = ({ carId, removeCar }: {
 		<Tooltip label={carId} fontSize='md'>
 			<Center>
 				<Button onClick={removeCar} variant='ghost' colorScheme='pink'>
-					<Spinner size='sm' speed='1s' /> Remove
+					Remove
 				</Button>
 			</Center>
 		</Tooltip>
@@ -43,18 +45,68 @@ const PlaceholderParkingSlotItem = ({ price, addCar }: {
 	</GridItem>
 }
 
-const ValetComponent = ({ valet, isBusy }: {
+const ValetComponent = ({ valet, isBusy, handleValetClick }: {
 	valet: Valet,
 	isBusy: boolean
+	handleValetClick: () => void
 }) => {
 	const avatarSrc = valet.nick === 'Tommy' ? Tommy : Sonny;
-	return (<Tooltip label={'Park with ' + valet.nick} fontSize='md'>
-		<Box position={'relative'} className='avatarBlock'>
+	const tooltip = isBusy ? valet.nick + ' is busy right now': 'Park with ' + valet.nick;
+	const clickHandler = isBusy ? () => {} : handleValetClick;
+	return (<Tooltip label={tooltip} fontSize='md'>
+		<Box position={'relative'} className='avatarBlock' onClick={clickHandler}>
 			<CircularProgress className={isBusy ? 'busyStatus' : 'busyStatus hidden'} color={'pink.400'} isIndeterminate size='120px' thickness='4px' />
 			<Avatar left='12px' top='12px' position={'absolute'} size='xl' name={valet.name} src={avatarSrc} />
 			<Center>{valet.nick}</Center>
 		</Box>
 	</Tooltip>);
+}
+
+const AudioPlayer = () => {
+	const [canplay, setCanPlay] = useState(false);
+	const [isPlaying, setPlaying] = useState(false);
+
+	const handlePlay = () => {
+		const player = document.getElementById('audioPlayer');
+		if (!player) {
+			return;
+		}
+		// @ts-ignore
+		player.volume = 0.5;
+		if (canplay && !isPlaying) {
+			// @ts-ignore
+			player.play();
+		} else {
+			// @ts-ignore
+			player.pause();
+			setPlaying(false);
+		}
+	}
+
+	return (
+		<>
+			<Switch colorScheme='pink' disabled={!canplay} size='lg' onChange={() => handlePlay()} />
+			<audio id='audioPlayer' src={audiofile} onPlaying={() => setPlaying(true)} onCanPlay={() => setCanPlay(true)} />
+		</>
+	)
+}
+
+const VoicePlayer = ({
+	voice,
+	play
+}:{
+	voice: string, play: boolean
+}) => {
+	// TODO: different voice for Sonny :)
+	const source = voice === 'Tommy' ? TommyEasy : TommyEasy;
+	const player = document.getElementById('voicePlayer');
+	let played = false;
+	if (voice && play && !played) {
+		// @ts-ignore
+		player.play();
+		played = true;
+	}
+	return <audio id='voicePlayer' src={source} />
 }
 
 const Parking = () => {
@@ -76,15 +128,50 @@ const Parking = () => {
 		}
 	]
 	const [busy, setBusy] = useState(['']);
+	const [voice, setVoice] = useState('')
+	const [voicePlayState, setPlayState] = useState(false)
+
+	const handleValetClick = ({ valet, price }: {
+		valet: string,
+		price?: number
+	}) => {
+		const carId = uniqueId();
+		dispatch(park({ carId, price }));
+		setBusy(() => {
+			let busyValets = [...busy];
+			if (!busyValets.includes(valet)) {
+				busyValets.push(valet);
+			}
+			return busyValets;
+		})
+		setVoice(valet);
+		setPlayState(true);
+		setTimeout(() => setPlayState(false), 1000);
+		setTimeout(() => setBusy(() => {
+			const busyValets = [...busy];
+			return busyValets.filter(busyValet => busyValet !== valet);
+		}), 5000);
+	}
 
 	return (
 		<>
 			<Center className='welcome'>Welcome back to the Veneto Hotel</Center>
 			<Container>
-				{/* <Heading>Free slots: {count}</Heading> */}
 				<AvatarGroup spacing={1} justifyContent='center'>
-					{valets.map(valet => <ValetComponent key={valet.name} valet={valet} isBusy={busy.includes(valet.nick)} />)}
+					{valets.map(valet => <ValetComponent
+						handleValetClick={() => handleValetClick({
+							valet: valet.nick,
+							price: !valet.isVIP ? cheapestSlot?.price : undefined
+						})}
+						key={valet.name}
+						valet={valet}
+						isBusy={busy.includes(valet.nick)} />)}
 				</AvatarGroup>
+
+				<Center>
+					<p className='welcome'>Play theme music</p>
+					<AudioPlayer />
+				</Center>
 
 				<SimpleGrid className='parkingArea' columns={2} spacing={5} background='whiteAlpha.600' padding='1rem'>
 					{allSlots.map((slot, index) => {
@@ -105,21 +192,15 @@ const Parking = () => {
 
 				<SimpleGrid columns={3} spacing={1}>
 					<GridItem>
-						<Button disabled={!canPark} onClick={() => {
-							const carId = uniqueId();
-							dispatch(park({ carId }));
-							setBusy(['Sonny']);
-							setTimeout(() => setBusy(['']), 5000);
-						}}>Park automatically</Button>
+						<Button disabled={!canPark} onClick={() => handleValetClick({
+							valet: 'Sonny'
+						})}>Park automatically</Button>
 					</GridItem>
 					<GridItem>
-						<Button disabled={!canPark} onClick={() => {
-							const carId = uniqueId();
-							const price = cheapestSlot?.price;
-							dispatch(park({ carId, price }));
-							setBusy(['Tommy']);
-							setTimeout(() => setBusy(['']), 5000);
-						}}>
+						<Button disabled={!canPark} onClick={() => handleValetClick({
+							price: cheapestSlot?.price,
+							valet: 'Tommy'
+						})}>
 							{'Park cheap: ' + CURR + cheapestSlot?.price}
 						</Button>
 					</GridItem>
@@ -127,6 +208,7 @@ const Parking = () => {
 						<Button onClick={() => dispatch(updatePrice())}>Randomize Prices</Button>
 					</GridItem>
 				</SimpleGrid>
+				<VoicePlayer voice={voice} play={voicePlayState} />
 			</Container>
 		</>
 	);
